fe31e9429a49169833fdacf4c5c985cf108b5409
Murilo Ribeiro <muriloribeiro11@gmail.com>
2026-04-21 14:08:26 -0300

    feat: Adiciona script de inicialização do banco de dados com itens do cardápio

---
+++ GoodHamburgerAPI.Infrastructure/DbInit/initDb.sql
@@ -0,0 +1,15 @@
+use goodhamburger_db;
+
+
+INSERT INTO tb_tipo_itens_cardapio VALUES 
+(1,'laches',1),
+(2,'acompanhamentos',1),
+(3,'bebidas',1);
+
+
+INSERT INTO tb_itens_cardapio VALUES 
+(1,'X Burger',1,1,5.50),
+(2,'X Bacon',1,1,7.00),
+(3,'X Egg',1,1,4.50),
+(4,'Batata Frita',2,1,2.00),
+(5,'Refrigerante',3,1,2.50);
